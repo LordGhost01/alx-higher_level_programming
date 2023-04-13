9ecff0bc30074e949c889ce9ca058aa815122fb2
LordGhost01 <ghostsmith01@yahoo.com>
2023-04-13 02:41:30 -0700

    final commit

---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Py test driven development project
---
+++ 0x07-python-test_driven_development/python.h
@@ -0,0 +1,14 @@
+#ifndef _PYTHON_H
+#define _PYTHON_H
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+{
+
+void print_python_string(PyObject *p);
+
+#endif
+
+}
