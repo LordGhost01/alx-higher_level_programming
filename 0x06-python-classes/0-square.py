416f8732792d439e2f0f6f0d0208c9b56398c188
LordGhost01 <ghostsmith01@yahoo.com>
2023-04-13 01:43:55 -0700

    final commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""docstring for Square"""
+
+
+class Square:
+    """docstring for init"""
+    def __init__(self):
+        pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""docstring for Square"""
+
+
+class Square:
+    """docstring for init"""
+    def __init__(self, size):
+        self.__size = size
